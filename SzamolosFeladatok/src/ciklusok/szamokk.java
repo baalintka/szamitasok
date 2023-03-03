bd2100024180cc357d60e1db132cf17b69d39dd3
baalintka <ballint3@gmail.com>
2023-03-03 12:35:17 +0100

    szamolas

---
+++ SzamolosFeladatok/src/ciklusok/szamokk.java
@@ -0,0 +1,19 @@
+
+package ciklusok;
+
+
+public class szamokk {
+    
+    
+    public static void main(String[] args) {
+    
+        for (int j = 2; j < 3; j++) {
+           for (int i = 2; i < 4; i++) {
+               System.out.println(i+""+j);
+               System.out.println(j+""+i);
+               
+           }
+        }
+    }
+    
+}
