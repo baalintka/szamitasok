3014abd42180b8ec1b11476b2ba230adec4a2970
baalintka <ballint3@gmail.com>
2023-03-03 13:37:18 +0100

    a

--- SzamolosFeladatok/src/ciklusok/szamokk.java
+++ SzamolosFeladatok/src/ciklusok/szamokk.java
@@ -1,19 +1,33 @@
-
 package ciklusok;
 
+import java.util.Scanner;
 
 public class szamokk {
-    
-    
+
+    private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-    
-        for (int j = 2; j < 3; j++) {
-           for (int i = 2; i < 5; i++) {
-               System.out.println(i+""+j);
-               System.out.println(j+""+i);
-               
-           }
+
+        System.out.println("ismetlessel");
+        boolean ismetel = false;
+        int ismDb = 0, ismNkdb = 0;
+        for (int j = 2; j <= 4; j++) {
+            for (int i = 2; i <= 4; i++) {
+                for (int k = 2; k <= 4; k++) {
+                    if (ismetel) {
+
+                        System.out.println("" + k + j + i);
+                    } else if (k != j && j != i && k != i) {
+                        System.out.println("" + k + j + i);
+                    }
+                }
+            }
+            if (ismetel) {
+                System.out.println("ismetlessel:" + ismDb);
+            } else {
+                System.out.println("ismetlessel:" + ismNkdb);
+            }
         }
+
     }
-    
 }
