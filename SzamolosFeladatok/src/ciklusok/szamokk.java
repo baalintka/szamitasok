5eca35ca4cc0431d49f820bff3ad7e1c3c24493b
baalintka <ballint3@gmail.com>
2023-03-03 12:37:36 +0100

    szamolass

--- SzamolosFeladatok/src/ciklusok/szamokk.java
+++ SzamolosFeladatok/src/ciklusok/szamokk.java
@@ -8,7 +8,7 @@ public class szamokk {
     public static void main(String[] args) {
     
         for (int j = 2; j < 3; j++) {
-           for (int i = 2; i < 4; i++) {
+           for (int i = 2; i < 5; i++) {
                System.out.println(i+""+j);
                System.out.println(j+""+i);
                
